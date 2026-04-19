5f1beb81a9be0404736e311406db50987e189b18
Harshita <harshita19062004@gmail.com>
2026-04-19 15:25:27 +0530

    Added LeetCode Daily Solution - Maximum Distance Between a Pair of Values (2026-04-19)

---
+++ 2026-04-19-maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,26 @@
+// Problem: Maximum Distance Between a Pair of Values
+// Link: https://leetcode.com/problems/maximum-distance-between-a-pair-of-values/
+// Date: 2026-04-19
+// Difficulty: Medium
+// Language: C++
+
+class Solution {
+public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+        int n = nums1.size() ;
+        int m = nums2.size();
+        int ans = 0;
+        while( i < n && j < m){
+            if(nums1[i] <= nums2[j]){
+                ans = max(ans,  j -i);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans == INT_MIN ? 0 : ans;
+    }
+};
